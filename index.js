cfcab799d876bb9d55155bec714e805837677645
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-06-04 17:47:22 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require("./server");
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://127.0.0.1/330-final-project', {}).then(() => {
+  server.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
+}).catch((e) => {
+  console.error(`Failed to start server:`, e);
+});
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const router = Router();
+
+router.use("/login", require("./login"));
+
+module.exports = router;
