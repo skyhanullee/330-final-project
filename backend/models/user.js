4ea90034e45ad6db24945db1c47907c20e39e698
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-06-10 03:07:30 -0700

    added savedJobs to users

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   roles: { type: [String], required: true },
+  savedJobs: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'jobs' }], required: true },
 });
 
 
