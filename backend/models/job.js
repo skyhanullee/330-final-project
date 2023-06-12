e8ef8107c6ba88f561a5bb7c8bf06cf81ed8974d
skyhanullee <leeskyha@gmail.com>
2023-06-12 03:47:45 -0700

    added author to schema and changed salary to required

--- backend/models/job.js
+++ backend/models/job.js
@@ -7,11 +7,12 @@ const jobSchema = new mongoose.Schema({
   description: { type: String, required: true },
   location: { type: String, required: true },
   company: { type: String },
-  salary: { type: Number },
+  salary: { type: Number, required: true },
   createdAt: { type: String },
   latitude: { type: Number },
   longitude: { type: Number },
   url: { type: String },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
 });
 
 // 
