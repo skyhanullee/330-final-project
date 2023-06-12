82b071afbdfcf29bfca21f67e1f82578b9fd53d1
skyhanullee <leeskyha@gmail.com>
2023-06-12 16:01:58 -0700

    added .env JWT_SECRET_KEY for jwt.verify instead of decode

--- backend/middleware/isAuthorized.js
+++ backend/middleware/isAuthorized.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const userDAO = require('../daos/user');
+require('dotenv').config();
 
 // It should verify the JWT provided in req.headers.authorization
 // and put the decoded value on the req object.
@@ -7,18 +8,18 @@ const isAuthorized = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     if (!token || !token.includes('Bearer')) {
-      res.status(401).send('No valid token.');
+      res.status(401).send({ message: 'No valid token.' });
     }
     else {
       const userToken = token.replace('Bearer ', '');
-      const authorizedUser = jwt.decode(userToken);
+      const authorizedUser = jwt.verify(userToken, process.env.JWT_SECRET_KEY);
       if (!authorizedUser) {
-        return res.status(401).send('Token from user does not exist.')
+        return res.status(401).send({ message: 'Token from user does not exist.' })
       }
 
       const isUserExists = userDAO.getUser(authorizedUser?._id);
       if (!isUserExists) {
-        return res.status(401).send('User does not exist.');
+        return res.status(401).send({ message: 'User does not exist.' });
       }
       else {
         req.user = {
