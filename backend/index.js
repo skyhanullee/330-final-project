9f2fd46ba8f15a0d9292c85bdf12459acbc011ab
skyhanullee <leeskyha@gmail.com>
2023-06-12 03:46:29 -0700

    changd mongo database

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, { dbName: `jobPosts` }).then(() => {
+mongoose.connect(process.env.MONGO_URI, { dbName: `jobSearch` }).then(() => {
   server.listen(port, () => {
     console.log(`Server is listening on http://127.0.0.1:${port}`);
   });
