e7153f6c17a7699b895b154e559ed94e87be5fb6
skyhanullee <leeskyha@gmail.com>
2023-06-13 16:04:22 -0700

    reorganized code

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -4,47 +4,19 @@ const jobDAO = require('../daos/job');
 const isAuthorized = require("../middleware/isAuthorized");
 const isPro = require("../middleware/isPro");
 
-// Create: POST /jobs - restricted to users with the "pro" role
-router.post("/", isAuthorized, isPro, async (req, res, next) => {
-  // router.post("/", async (req, res, next) => {
+// Get all jobs: GET /jobs - open to all users
+router.get("/", async (req, res, next) => {
+  // router.get("/", async (req, res, next) => {
   try {
-    const job = req.body;
-    if (!job) {
-      res.status(400).send({ message: 'No job given.' });
-    }
-
-    const existingJob = await jobDAO.getJobByJobId(job.jobId);
-    if (existingJob) {
-      return res.status(409).send({ message: 'Job already saved.' });
-    }
-
-    const newJob = await jobDAO.createJob(job);
-    res.status(200).json(newJob);
-    console.log(newJob);
-
+    const jobs = await jobDAO.getAllJobs();
+    res.json(jobs);
+    // res.json({ msg: "get works!" });
   } catch (e) {
     next(e);
   }
 });
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const job = req.body;
-//     if (!job) {
-//       res.status(400).send('No job given.');
-//     }
-//     else {
-//       const newJob = await jobDAO.createJob(job);
-//       res.json(newJob, {
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       });
-//       console.log(newJob);
-//     }
-//   } catch (e) {
-//     next(e);
-//   }
-// });
+
+// router.use(isAuthorized);
 
 // Get specific job: GET /jobs/:id - open to all users
 router.get("/:id", isAuthorized, async (req, res, next) => {
@@ -63,20 +35,36 @@ router.get("/:id", isAuthorized, async (req, res, next) => {
   }
 });
 
-// Get all jobs: GET /jobs - open to all users
-router.get("/", async (req, res, next) => {
-  // router.get("/", async (req, res, next) => {
+
+// Create: POST /jobs - restricted to users with the "pro" role
+// router.post("/", isAuthorized, isPro, async (req, res, next) => {
+router.post("/", isAuthorized, async (req, res, next) => {
+  // router.post("/", async (req, res, next) => {
   try {
-    const jobs = await jobDAO.getAllJobs();
-    res.json(jobs);
-    // res.json({ msg: "get works!" });
+    const job = req.body;
+    console.log(job);
+    if (!job) {
+      res.status(400).send({ message: 'No job given.' });
+    }
+
+
+    const existingJob = await jobDAO.getJobByJobId(job.jobId);
+    if (existingJob) {
+      return res.status(409).send({ message: 'Job already saved.' });
+    }
+
+    const newJob = await jobDAO.createJob(job);
+    res.status(200).json(newJob);
+    console.log(newJob);
+
   } catch (e) {
     next(e);
   }
 });
 
 // Update a note: PUT /jobs/:id - restricted to users with the "pro" role
-router.put("/:id", isAuthorized, isPro, async (req, res, next) => {
+// router.put("/:id", isAuthorized, isPro, async (req, res, next) => {
+router.put("/:id", isAuthorized, async (req, res, next) => {
   // router.put("/:id", async (req, res, next) => {
   try {
     const jobId = req.params.id;
