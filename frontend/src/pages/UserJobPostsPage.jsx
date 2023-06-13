fb2a44872a9c204f05a3d077d028ca5bf6115343
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-06-13 05:10:50 -0700

    initial commit

---
+++ frontend/src/pages/UserJobPostsPage.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import JobCard from "../components/JobCard";
+import { Link } from "react-router-dom";
+
+function UserJobPosts() {
+  const [jobPosts, setJobPosts] = useState([]);
+  const getAllJobPosts = () => {
+    fetch('http://127.0.0.1:4000/jobs', {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        setJobPosts(data);
+      });
+
+  };
+  console.log(jobPosts);
+  useEffect(() => {
+    getAllJobPosts();
+  }, []);
+
+  // getAllJobPosts();
+
+  const jobPostsList = jobPosts.map((jobPost) => {
+    return (
+      // <li>{jobPost.title}</li>
+      <li key={jobPost._id}>
+
+        <JobCard job={jobPost} />
+      </li>
+    )
+  })
+
+  return (
+    <div className="page">
+      <h1>User Job Posts</h1>
+      <button><Link to='/createJob'>Add a Job Post</Link></button>
+      <ul>{jobPostsList}</ul>
+    </div>
+  )
+}
+export default UserJobPosts
